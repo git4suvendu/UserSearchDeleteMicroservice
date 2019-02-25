397820f43e483a9a5aecd9709a0f0073bb2319e3
Suvendu Mandal <m_andal2002@yahoo.co.in>
2019-02-25 23:28:37 +0000

    Firs commit

---
+++ src/main/java/com/microservice/user/registration/UserSearchApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.user.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSearchApplication.class, args);
+	}
+
+}
